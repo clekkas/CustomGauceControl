755edb2b4c9e9cf05e161fa608084a6488906702
Chris Lekkas <clekkas@port-amcken01.corp.checkfree.com>
2018-12-17 10:25:22 -0800

    Initial Commit

---
+++ Custom Gauge Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Gauge Control
+//
+//  Created by Lekkas, Chris (Hillsboro) on 12/17/18.
+//  Copyright © 2018 Lekkas, Chris (Hillsboro). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
